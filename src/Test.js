7f91c0cf9db2dfd09b7e964b8d0402de92d68fee
hamzaabdous <abdoushamza6@gmail.com>
2021-09-25 21:26:47 +0100

    2 Components (Person & Message)

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const Test = () => {
+  var name = "Hamza Abdous";
+  var message = "this is my message";
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Person name={name} />
+      <Message message={message} />
+    </>
   );
 };
+const Person = (props) => {
+  return <h2>{props.name}</h2>;
+};
 
+const Message = (props) => {
+  return <p>{props.message}</p>;
+};
 export default Test;
